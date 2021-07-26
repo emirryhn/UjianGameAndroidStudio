1e9fe64f3727bcbaaae4e205f423bf8fc8c50f0c
Emir Rayhan <emir123.er@gmail.com>
2021-07-26 20:05:58 +0700

    second commit, additional array logic

--- app/src/main/java/com/example/ujiangameandroid/MainActivity.java
+++ app/src/main/java/com/example/ujiangameandroid/MainActivity.java
@@ -13,16 +13,10 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton btnUp, btnDown, btnRight, btnLeft;
-    private CardView[] card = new CardView[16];
+    private CardView[][] card = new CardView[4][4];
 
-    int[][] movement = {
-            {0,1,2,3}, {4,5,6,7}, {8,9,10,11}, {12,13,14,15},//rows
-            {0,4,8,12}, {2,5,9,13}, {2,6,10,14}, {3,7,11,15},//columns
-    };
-
-    //black => 0
-    //white => 1
-    int[] blankCard = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int x=0;
+    int y=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,21 +30,26 @@ public class MainActivity extends AppCompatActivity {
 
         //identify all cards id number
         for(int i = 0; i < card.length; i++){
-            String cardID = "card_" + i;
-            //to get id resource(necessary)
-            int resourceID = getResources().getIdentifier(cardID,"id",getPackageName());
-            card[i] = findViewById(resourceID);
+            for(int j = 0; j<card.length;j++){
+                String cardID = "card_" + i+j;
+                card[i][j] = findViewById(getResources().getIdentifier(cardID,"id",getPackageName()));
+            }
         }
+
+        card[0][0].setCardBackgroundColor(Color.BLACK);
 
         btnUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             Log.i("test", "button up");
-
-            String cardID = v.getResources().getResourceEntryName(v.getId());
-            int gameStart = Integer.parseInt(cardID.substring(cardID.length()-1), cardID.length());
-            v.setBackgroundColor(Color.parseColor("#FF000000"));
-            blankCard[gameStart] = 1;
+            if((y<=3) && (y>0)){
+                y=y-1;
+            }
+                if (card[y][x].getCardBackgroundColor().getDefaultColor() == Color.BLACK) {
+                    card[y][x].setCardBackgroundColor(Color.WHITE);
+                } else {
+                    card[y][x].setCardBackgroundColor(Color.BLACK);
+                }
             }
 
         });
@@ -58,18 +57,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i("test", "button down");
+                if ((y < 3) && (y >= 0)) {
+                    y = y + 1;
+                }
+                if (card[y][x].getCardBackgroundColor().getDefaultColor() == Color.BLACK) {
+                    card[y][x].setCardBackgroundColor(Color.WHITE);
+                } else {
+                    card[y][x].setCardBackgroundColor(Color.BLACK);
+                }
             }
         });
         btnLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("test", "button left");
+                if((x<=3) && (x>0)){
+                    x=x-1;
+
+                }if (card[y][x].getCardBackgroundColor().getDefaultColor() == Color.BLACK) {
+                    card[y][x].setCardBackgroundColor(Color.WHITE);
+                } else {
+                    card[y][x].setCardBackgroundColor(Color.BLACK);
+                }
             }
         });
         btnRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("test", "button right");
+
+                if((x<3)&&(x>=0)){
+                    x=x+1;
+                }if (card[y][x].getCardBackgroundColor().getDefaultColor() == Color.BLACK) {
+                    card[y][x].setCardBackgroundColor(Color.WHITE);
+                } else {
+                    card[y][x].setCardBackgroundColor(Color.BLACK);
+                }
             }
         });
 
